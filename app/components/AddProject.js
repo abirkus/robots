2c31c6284516a351c947a300c63d9ee642fbc434
Andre Birkus <birkusandre@gmail.com>
2020-05-08 16:21:57 -0500

    updating add project fform

--- app/components/AddProject.js
+++ app/components/AddProject.js
@@ -1,123 +1,101 @@
 import React, {useState} from 'react';
 import { useDispatch} from 'react-redux';
 import {addProjectThunk} from '../redux/projects.js';
-import { DatePicker, Button } from 'antd';
+import { Form, Input, Select, Button, Radio, DatePicker } from 'antd';
+
+const layout = {
+	labelCol: { span: 8 },
+	wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+	wrapperCol: {
+	  offset: 8,
+	  span: 16,
+	},
+  };
+  const { Option } = Select;
+
+
 
 function AddProject(props) {
 	const dispatch = useDispatch()
-	const [title, setTitle] = useState('')
-	const [deadline, setDeadline] = useState('')
-	const [priority, setPriority] = useState('')
-	const [completed, setCompleted] = useState(false)
-	const [description, setDescription] = useState('')
 
-	const handleTitleChange = (evt) => {
-		setTitle(evt.target.value)
-	}
+	const [form] = Form.useForm();
 
-	const handleDeadlineChange = (evt) => {
-		setDeadline(evt.target.value)
-	}
-
-	const handlePriorityChange = (evt) => {
-		setPriority(evt.target.value)
-	}
-
-	const handleCompletedChange = (evt) => {
-		setCompleted(evt.target.value)
-	}
-
-	const handleDescriptionChange = (evt) => {
-		setDescription(evt.target.value)
-	}
-
-	const handleSubmit = evt => {
-		evt.preventDefault();
+	const onFinish = values => {
 		try {
-			dispatch(addProjectThunk({title, deadline, priority, completed, description}))
-			setTitle('')
-			setDeadline('')
-			setPriority('')
-			setCompleted(false)
-			setDescription('')
+			console.log("values", values)
+			//dispatch({type: 'ADD_PROJ', value: values.project})
+			form.resetFields()
 		} catch (e) {
 			console.error(e.message)
 		}
-		
-
 	}
+
+	const onReset = () => {
+		form.resetFields();
+	  };
 
 	return (
 		<div className="form">
 			<h1>Add a new project</h1>
-			<form id="project-form" onSubmit={handleSubmit}>
-				<label htmlFor="title">
-					Project Title:
-					{!title ? (
-						<span className="warning">
-							{'This field is required'}
-						</span>
-					) : (
-						<span />
-					)}
-				</label>
-				<input
-					name="title"
-					type="text"
-					onChange={handleTitleChange}
-					value={title}
-				/>
+			<Form
+					name="addProject"
+					form={form}
+					onFinish={onFinish}
+					>
+				<Form.Item
+					label="Project Title:"
+					name = {['project', 'title' ]}
+					rules={[{ required: true, message: 'Please input project title!' }]}
+					>
+					<Input />
+				</Form.Item>
 
-				<label htmlFor="deadline">Deadline:</label>
-				<DatePicker value={deadline} onChange={handleDeadlineChange} />
-				{/* <input
-					name="deadline"
-					type="date"
-					onChange={handleDeadlineChange}
-					value={deadline}
-				/> */}
+				<Form.Item
+					label="Deadline:"
+					name = {['project', 'deadline']}
+					>
+					<DatePicker />
+				</Form.Item>
 
-				<label htmlFor="priority">Priority:</label>
-				<select
-					defaultValue="none"
-					name="priority"
-					type="number"
-					onChange={handlePriorityChange}>
-					<option value="none" />
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
-					<option value="9">9</option>
-					<option value="10">10</option>
-				</select>
+				<Form.Item label="Priority" name={['project', 'priority']}>
+					<Select>
+						<Option value="1">1</Option>
+						<Option value="2">2</Option>
+						<Option value="3">3</Option>
+						<Option value="4">4</Option>
+						<Option value="5">5</Option>
+						<Option value="6">6</Option>
+						<Option value="7">7</Option>
+						<Option value="8">8</Option>
+						<Option value="9">9</Option>
+						<Option value="10">10</Option>
+					</Select>
+				</Form.Item>
 
-				<label htmlFor="completed">Completed:</label>
-				<select
-					defaultValue="false"
-					name="completed"
-					type="text"
-					onChange={handleCompletedChange}>
-					<option value="true">Yes</option>
-					<option value="false">No</option>
-				</select>
+				<Form.Item
+					name={['project', 'completed']} 
+					label="Completed:">
+					<Radio.Group defaultValue="false">
+						<Radio value="true">Yes</Radio>
+						<Radio value="false">No</Radio>
+					</Radio.Group>
+				</Form.Item>
 
-				<label htmlFor="description">Description:</label>
-				<input
-					name="description"
-					type="text"
-					onChange={handleDescriptionChange}
-					value={description}
-				/>
+				<Form.Item label="Description:" name={['project', 'description']}>
+					<Input />
+				</Form.Item>
 
-				<Button type="primary" disabled={!title}>
-					Submit
-				</Button>
-			</form>
+				<Form.Item {...tailLayout}>  
+					<Button type="primary"  htmlType="submit">
+						Submit
+					</Button>
+					<Button htmlType="button" onClick={onReset} type="danger">
+						Reset
+					</Button>
+				</Form.Item>
+			</Form>
 		</div>
 	);
 
@@ -125,12 +103,3 @@ function AddProject(props) {
 
 export default AddProject
 
-// const mapDispatchToProps = dispatch => {
-// 	return {
-// 		post: obj => dispatch(addProjectThunk(obj)),
-// 	};
-// };
-// export default connect(
-// 	null,
-// 	mapDispatchToProps
-// )(AddProject);
