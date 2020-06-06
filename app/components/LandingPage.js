da8a6bbc6d4afc05f6b0c95765124ecee38e95de
Andre Birkus <birkusandre@gmail.com>
2020-06-06 11:03:31 -0500

    parallax and employee animations

--- app/components/LandingPage.js
+++ app/components/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Greeting from './scrollMagic/greeting.js'
 import Parallax from './scrollMagic/parallax.js'
+import Employee from './scrollMagic/employee.js'
 
 const LandingPage = () => {
 	return (
 		<div className="greeting">
 			<Greeting />
+			<Employee />
 			<Parallax />
 		</div>
 	);
--- app/components/scrollMagic/greeting.js
+++ app/components/scrollMagic/greeting.js
@@ -1,21 +1,21 @@
 import React, {useRef, useEffect} from 'react';
-
+import {TweenMax, Power3} from 'gsap'
 import ScrollMagic from "scrollmagic/scrollmagic/uncompressed/ScrollMagic";
 import "animation.gsap";
 import "debug.addIndicators";
-import TweenMax from "gsap/src/uncompressed/TweenMax";
+//import TweenMax from "gsap/src/uncompressed/TweenMax";
 import TimelineMax from "gsap/src/uncompressed/TimelineMax";
 
 const Greeting = () => {
 	let logoItem = useRef(null)
-
 	const controller = new ScrollMagic.Controller({addIndicators: true})
-
-
-
-
+	
 	useEffect(() => {
-		const ourScene = new ScrollMagic.Scene({triggerElement: '.panda-start'})
+		const ourScene = new ScrollMagic.Scene({
+			triggerElement: '.panda-start',
+			triggerHook: 0,
+			duration: '50%'
+		})
 		.setTween(TweenMax.to(logoItem, 0.9, {opacity: 1, y: -20, ease: Power3.easeOut}))
 		.addIndicators({
 			name: 'fade scene',
