fc41f4f7b343b449996a95d03791dc472836fdc9
Andre Birkus <birkusandre@gmail.com>
2020-06-06 10:09:13 -0500

    re-installing gsap. importing gsap in webpack. scrollmagic and gsap now functional

--- app/components/scrollMagic/greeting.js
+++ app/components/scrollMagic/greeting.js
@@ -1,11 +1,10 @@
 import React, {useRef, useEffect} from 'react';
-//import {TweenMax, Power3} from 'gsap'
-//import ScrollMagic from 'scrollmagic'
-import ScrollMagic from "scrollmagic/scrollmagic/uncompressed/ScrollMagic";
-import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
-// import TweenMax from "gsap/src/uncompressed/TweenMax";
-// import TimelineMax from "gsap/src/uncompressed/TimelineMax";
 
+import ScrollMagic from "scrollmagic/scrollmagic/uncompressed/ScrollMagic";
+import "animation.gsap";
+import "debug.addIndicators";
+import TweenMax from "gsap/src/uncompressed/TweenMax";
+import TimelineMax from "gsap/src/uncompressed/TimelineMax";
 
 const Greeting = () => {
 	let logoItem = useRef(null)
@@ -16,34 +15,24 @@ const Greeting = () => {
 
 
 	useEffect(() => {
-		// TweenMax.to(
-		// 	logoItem,
-		// 	0.9,
-		// 	{
-		// 		opacity: 1,
-		// 		y: -20,
-		// 		ease: Power3.easeOut
-		// 	}
-		// )
-		const ourScene = new ScrollMagic.Scene({
-			triggerElement: '.greetingStart'
-		})
-		.setClassToggle('#logo', 'fade-in')
+		const ourScene = new ScrollMagic.Scene({triggerElement: '.panda-start'})
+		.setTween(TweenMax.to(logoItem, 0.9, {opacity: 1, y: -20, ease: Power3.easeOut}))
 		.addIndicators({
 			name: 'fade scene',
 			colorTrigger: 'black',
-			indent: 200
 		})
 		.addTo(controller)
 	}, [])
 
 	return (
-		<div className="greetingStart">
+		<div >
+			<div className="greetingStart">
 			<h1>
 			Welcome to StackBot Project Management: your robot employees are
 			awaiting project assignments!
 			</h1>
-		<div>
+			</div>
+		<div className="panda-start">
 			<img ref={el => {logoItem = el}} src="/panda.gif" id="logo" className="app-logo" alt="logo" />
 		</div>
 		</div>
