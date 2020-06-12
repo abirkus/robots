89aa9b3621720d56574b5ad7260a2ae8c96682cd
Andre Birkus <birkusandre@gmail.com>
2020-06-11 21:37:59 -0500

    styling

--- app/components/scrollMagic/welcome.js
+++ app/components/scrollMagic/welcome.js
@@ -6,19 +6,27 @@ import "debug.addIndicators";
 
 const Welcome = () => {
   let background = useRef(null)
-  
+  let imageL = useRef(null)
   useEffect(() => {
-    TweenMax.to(background, 2, {
+    TweenMax.to(background, 1, 
+      {
       delay: 2,
-      opacity: 1,
-      ease: RoughEase.easeOut
+      css: { scale: 0, borderRadius: '50% 50% 50% 50%'},
+      ease: Power0.easeInOut
     })
+    TweenMax.to(imageL, 2,
+      {
+      delay: 3,
+      opacity: 1,
+      ease: Power3.easeOut
+    })
+
   }, [])
 
 
   return (
-    <div className="welcomeWrapper">
-      <div ref={el => {background = el}} className="welcome">
+    <div  ref={el => {imageL = el}}  className="welcome">
+      <div  ref={el => {background = el}} className="welcomeWrapper">
           <h1 className="welcomeText">Welcome To StackBot Inc.</h1>
       </div>
     </div>
