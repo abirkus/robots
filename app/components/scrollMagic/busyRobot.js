04b010dc18ea9a5e6baff3538e8bf39a3d6bdd7f
Andre Birkus <birkusandre@gmail.com>
2020-07-15 12:29:34 -0500

    adding eyes

--- app/components/scrollMagic/busyRobot.js
+++ app/components/scrollMagic/busyRobot.js
@@ -9,11 +9,9 @@ var posX = 0,
 	posY = 0
 
 const BusyRobot = () => {
-	// let logoItem = useRef(null)
 	let follower = useRef(null)
 	let cursor = useRef(null)
-	const controller = new ScrollMagic.Controller({addIndicators: true})
-
+	let eyesCanvas = useRef(null)
 	const [position, setPosition] = useState({x: 0, y: 0})
 
 	var mouseX = 0,
@@ -24,8 +22,6 @@ const BusyRobot = () => {
 			setPosition({x: e.clientX, y: e.clientY})
 			mouseX = e.clientX - 30
 			mouseY = e.clientY - 15
-			// posX += (mouseX - posX) / 9
-			// posY += (mouseY - posY) / 9
 		}
 
 		window.addEventListener('mousemove', setFromEvent)
@@ -56,40 +52,6 @@ const BusyRobot = () => {
 		}
 	}, [])
 
-	// useEffect(() => {
-	// const ourScene = new ScrollMagic.Scene({
-	// 	triggerElement: '.panda-start',
-	// 	triggerHook: 0,
-	// 	duration: '100%',
-	// })
-	// 	.setTween(
-	// 		TweenMax.to(logoItem, 0.9, {
-	// 			opacity: 1,
-	// 			y: '50%',
-	// 			ease: Power3.easeOut,
-	// 		})
-	// 	)
-	// 	.addIndicators({
-	// 		name: 'fade scene',
-	// 		colorTrigger: 'black',
-	// 	})
-	// 	.addTo(controller)
-	//	}, [])
-
-	// $(document).on('mousemove', function (e) {
-	// 	mouseX = e.clientX
-	// 	mouseY = e.clientY
-	// })
-	// // yellow circle
-	// $('.link').on('mouseenter', function () {
-	// 	cursor.addClass('active')
-	// 	follower.addClass('active')
-	// })
-	// $('.link').on('mouseleave', function () {
-	// 	cursor.removeClass('active')
-	// 	follower.removeClass('active')
-	// })
-
 	return (
 		<div className='busyRobot'>
 			<div className='cursor' ref={cursor}>
@@ -98,12 +60,65 @@ const BusyRobot = () => {
 			<div className='follower' ref={follower}>
 				<img src='/target.png' className='targetLogo' />
 			</div>
-			<h1>Exmplore more features by following navbar links up</h1>
-			<h1>
-				{position.x}:{position.y}
-			</h1>
+			<svg id='svg' width='300' height='400'>
+				<g id='left-eye'>
+					<circle className='eye-outer' cx='68' cy='308' r='45' />
+					<circle className='eye-iris' cx='68' cy='308' r='30' />
+					<circle className='eye-inner' cx='68' cy='308' r='20' />
+				</g>
+				<g id='right-eye'>
+					<circle className='eye-outer' cx='230' cy='308' r='45' />
+					<circle className='eye-iris' cx='230' cy='308' r='30' />
+					<circle className='eye-inner' cx='230' cy='308' r='20' />
+				</g>
+				<text x='0' y='15' fill='red'>
+					Exmplore more features by following navbar links up
+					{position.x}:{position.y}
+				</text>
+			</svg>
 		</div>
 	)
 }
 
 export default BusyRobot
+
+// function eyes(x, y, size, event) {
+// 	var canvas = document.createElement('canvas'),
+// 		context = canvas.getContext('2d')
+// 	document.body.appendChild(canvas)
+// 	canvas.style.position = 'absolute'
+// 	canvas.style.left = x - size - 5 + 'px'
+// 	canvas.style.top = y - size / 2 - 5 + 'px'
+
+// 	var rect = canvas.getBoundingClientRect()
+// 	canvas.width = size * 2 + 10
+// 	canvas.height = size + 10
+
+// 	onMouseMove(event)
+
+// 	document.addEventListener('mousemove', onMouseMove)
+
+// 	function onMouseMove(event) {
+// 		var x = event.clientX - rect.left,
+// 			y = event.clientY - rect.top
+// 		context.clearRect(0, 0, size * 2 + 10, size + 10)
+// 		drawEye(x, y, size / 2 + 5, size / 2 + 5)
+// 		drawEye(x, y, size * 1.5 + 5, size / 2 + 5)
+// 	}
+
+// 	function drawEye(x, y, cx, cy) {
+// 		var dx = x - cx,
+// 			dy = y - cy,
+// 			angle = Math.atan2(dy, dx)
+// 		context.save()
+// 		context.translate(cx, cy)
+// 		context.rotate(angle)
+// 		context.beginPath()
+// 		context.arc(0, 0, size / 2, 0, Math.PI * 2)
+// 		context.stroke()
+// 		context.beginPath()
+// 		context.arc(size * 0.4, 0, size * 0.1, 0, Math.PI * 2)
+// 		context.fill()
+// 		context.restore()
+// 	}
+// }
